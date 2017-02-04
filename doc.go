5faa9f0158493c1e57ea399911aa201d80724f49
pd0mz <pd0mz@brandmeister.network>
2017-02-04 21:37:50 +0100

    Added doc.go

---
+++ doc.go
@@ -0,0 +1,9 @@
+/*
+Package brandmeister contains protocols for interacting with the BrandMeister
+DMR Master Server.
+
+Note: in order to make use of this framework you must have a CEPT (Conference of
+Postal and Telecommunications Administrations) recognised amateur radio
+license in compliance with CEPT T/R 61-01 or ECC/REC/(05) 06.
+*/
+package brandmeister
